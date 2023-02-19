2446596d870d016b410b9633cef0cac1e778b89b
poojakrp777 <poojakrp777@gmail.com>
2023-02-19 15:55:04 +0530

    [POOJA] Ability to create the binary search tree shown in figure

--- BinarySearchTree/BinarySearchTree.cs
+++ BinarySearchTree/BinarySearchTree.cs
@@ -43,13 +43,19 @@ namespace BinarySearchTree
         {
             if (this.LeftTree != null)
             {
+                this.leftCount++;
                 this.LeftTree.Display();
             }
             Console.WriteLine(this.NodeData.ToString());
             if (this.RightTree != null)
             {
+                this.rightCount++;
                 this.RightTree.Display();
             }
+        }
+        public void GetSize()
+        {
+            Console.WriteLine("Size of BST is = " + (1 + this.leftCount + this.rightCount));
         }
     }
 }
